f6d34b54ca0b1b9f6ba6ecd21f8c577bc4f9a104
ashuksu <ashuksu@gmail.com>
2025-04-29 23:09:58 +0200

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], (Controller) => {
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+], (Controller, MessageToast) => {
    "use strict";
 
    return Controller.extend("project_d.controller.App", {
       onShowHello() {
-         alert("Hello World 3");
+        MessageToast.show("Hello World 4")
       }
    });
 });
