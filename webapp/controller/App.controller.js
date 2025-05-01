ea27d6106977d80507aeb0fbd942b32d2a2a52f1
ashuksu <ashuksu@gmail.com>
2025-05-01 12:32:44 +0200

    Step 8: Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,23 +3,50 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  (Controller, MessageToast, JSONModel) => {
+  (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
 
     return Controller.extend("project_d.controller.App", {
       onInit() {
+        const oView = this.getView();
         const oData = {
           recipient: {
-            name: "worldName",
+            name: "Name",
+            do: "Press here",
           },
         };
         const oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-        // this.getView().setModel(new JSONModel({}), "ui");
+        oView.setModel(oModel);
+
+        const i18nModel = new ResourceModel({
+          bundleName: "project_d.i18n.i18n",
+        });
+        oView.setModel(i18nModel, "i18n");
+      },
+      formatting(sText, ...args) {
+        if (!sText) {
+          return;
+        }
+
+        for (let i = 0; i < args.length; i++) {
+          if (sText.includes(`{${i}}`)) {
+            sText = sText.replace(`{${i}}`, args[i]);
+          }
+        }
+
+        return sText;
       },
       onShowHello() {
-        MessageToast.show("Hello World 4");
+        const oView = this.getView();
+        const oData = oView.getModel();
+        const oBundl = oView.getModel("i18n").getResourceBundle();
+
+        const sRecipientName = oData.getProperty("/recipient/name");
+        const sMsg = oBundl.getText("buttonTextsMsg", [sRecipientName]);
+
+        MessageToast.show(sMsg);
       },
     });
   }
