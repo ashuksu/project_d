aad663ea47a1d8e93ed8e43b8c0082ea488f5cc0
Ashuksu <ashuksu@gmail.com>
2025-07-20 15:45:38 +0200

    main - refactored: simplify controller syntax and improve readability

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,16 +1,16 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
-  function (Controller, JSONModel) {
-    "use strict";
+    ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+    (Controller, JSONModel) => {
+        "use strict";
 
-    return Controller.extend("project_d.controller.InvoiceList", {
-      onInit() {
-        const oViewModel = new JSONModel({
-          currency: "EUR",
+        return Controller.extend("project_d.controller.InvoiceList", {
+            onInit() {
+                const oViewModel = new JSONModel({
+                    currency: "EUR",
+                });
+
+                this.getView().setModel(oViewModel, "view");
+            },
         });
-
-        this.getView().setModel(oViewModel, "view");
-      },
-    });
-  }
+    }
 );
