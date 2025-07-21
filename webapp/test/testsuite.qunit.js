34417c99ad905edd06071667ee3b24e8def7f998
Ashuksu <ashuksu@gmail.com>
2025-07-21 10:03:24 +0200

    Step 27: Unit Test with QUnit & Step 26 (test/)

---
+++ webapp/test/testsuite.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.define(() => {
+    "use strict";
+    return {
+        name: "QUnit test suite for Project D",
+        defaults: {
+            page: "ui5://test-resources/project_d/Test.qunit.html?testsuite={suite}&test={name}",
+            qunit: {
+                version: 2
+            },
+            ui5: {
+                theme: "sap_horizon"
+            },
+            loader: {
+                paths: {
+                    "project_d": "../"
+                }
+            }
+        },
+        tests: {
+            "unit/unitTests": {
+                title: "Project D - Unit Tests",
+                module: "./unit/model/formatter"
+            }
+        }
+    };
+});
