bf8d075711bffd9f00ce3bf71c6ac31748f93546
Ashuksu <ashuksu@gmail.com>
2025-07-23 17:55:06 +0200

    Step 10: Descriptor for Applications (TypeScript)

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -1,16 +1,15 @@
-import Control from "sap/ui/core/Control";
 import UIComponent from "sap/ui/core/UIComponent";
-import XMLView from "sap/ui/core/mvc/XMLView";
 import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 /**
  * @namespace project_d
  */
 export default class Component extends UIComponent {
     public static metadata = {
-        "interfaces": ["sap.ui.core.IAsyncContentCreation"]
+        "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+        "manifest": "json"
     };
+
     init(): void {
         super.init();
 
@@ -21,17 +20,5 @@ export default class Component extends UIComponent {
         };
         const dataModel = new JSONModel(data);
         this.setModel(dataModel);
-
-        const i18nModel = new ResourceModel({
-            bundleName: "project_d.i18n.i18n"
-        });
-        this.setModel(i18nModel, "i18n");
-    };
-
-    createContent(): Control | Promise<Control | null> | null {
-        return XMLView.create({
-            "viewName": "project_d.view.App",
-            "id": "app"
-        });
     };
 };
