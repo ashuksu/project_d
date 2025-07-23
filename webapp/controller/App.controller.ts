0ea73dbee960c0d806f150098bdfa566022ff4fb
Ashuksu <ashuksu@gmail.com>
2025-07-23 16:31:47 +0200

    Step 5: Controllers (TypeScript)

---
+++ webapp/controller/App.controller.ts
@@ -0,0 +1,10 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @name project_d.controller.App
+ */
+export default class AppController extends Controller {
+    onShowHello(): void {
+        alert("Hello World from me!");
+     }
+};
