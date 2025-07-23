ff728910a409c171a0ae6f20ccbb598dd5263d06
Ashuksu <ashuksu@gmail.com>
2025-07-23 16:56:17 +0200

    Step 7: JSON Model (TypeScript)

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,11 +1,23 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 /**
  * @name project_d.controller.App
  */
 export default class AppController extends Controller {
+    onInit(): void {
+        const data = {
+            recipient: {
+                name: "WORLD"
+            }
+        };
+
+        const dataModel = new JSONModel(data);
+        this.getView()?.setModel(dataModel);
+    }
+
     onShowHello(): void {
         MessageToast.show("Hello World from me!");
-     }
+    }
 };
