dfe5385ab79523c60a6be9e4c9f50b0ff9269c40
Ashuksu <ashuksu@gmail.com>
2025-07-23 21:15:21 +0200

    Step 15: Nested Views (TypeScript)

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,18 +1,7 @@
 import Controller from "sap/ui/core/mvc/Controller";
-import MessageToast from "sap/m/MessageToast";
-import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
-import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
 /**
- * @name project_d.controller.App
+ * @namespace project_d.controller.App
  */
 export default class AppController extends Controller {
-    onShowHello(): void {
-        const recipient = (<JSONModel> this.getView()?.getModel())?.getProperty("/recipient/name");
-        const resourceBundle = <ResourceBundle> (<ResourceModel> this.getView()?.getModel("i18n"))?.getResourceBundle();
-        const msg = resourceBundle.getText("helloMsg", [recipient]) || "no text defined";
-
-        MessageToast.show(msg);
-    }
 };
