d379ca3cfa82b105fb52ef675739c6beca5c584a
Ashuksu <ashuksu@gmail.com>
2025-07-24 18:30:47 +0200

    Step 32: Routing Back and History (TypeScript)

--- webapp/controller/Detail.controller.ts
+++ webapp/controller/Detail.controller.ts
@@ -1,6 +1,7 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
-import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
+import {Route$PatternMatchedEvent} from "sap/ui/core/routing/Route";
+import History from "sap/ui/core/routing/History";
 
 /**
  * @namespace project_d.controller
@@ -10,12 +11,24 @@ export default class Detail extends Controller {
     onInit(): void {
         const router = UIComponent.getRouterFor(this);
         router.getRoute("detail").attachPatternMatched(this.onObjectMatched, this);
-    }
+    };
 
     onObjectMatched(event: Route$PatternMatchedEvent): void {
         this.getView().bindElement({
-            path: "/" + window.decodeURIComponent( (event.getParameter("arguments") as any).invoicePath),
+            path: "/" + window.decodeURIComponent((event.getParameter("arguments") as any).invoicePath),
             model: "invoice"
         });
-    }
+    };
+
+    onNavBack(): void {
+        const history = History.getInstance();
+        const previousHash = history.getPreviousHash();
+
+        if (previousHash !== undefined) {
+            window.history.go(-1);
+        } else {
+            const router = UIComponent.getRouterFor(this);
+            router.navTo("overview", {}, true);
+        }
+    };
 };
